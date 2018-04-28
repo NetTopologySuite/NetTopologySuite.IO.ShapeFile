7ee1e5c5e9190a9f97b873d9c53fd7824d54a65e
Joe Amenta <airbreather@linux.com>
2018-04-28 10:19:14 -0400

    Just whitespace changes

--- NetTopologySuite.IO.GeoTools/BigEndianBinaryWriter.cs
+++ NetTopologySuite.IO.GeoTools/BigEndianBinaryWriter.cs
@@ -5,48 +5,48 @@ using System.Text;
 
 namespace NetTopologySuite.IO
 {
-	/// <summary>
-    /// Extends the <see cref="BinaryWriter" /> class to allow the writing of integers 
+    /// <summary>
+    /// Extends the <see cref="BinaryWriter" /> class to allow the writing of integers
     /// and double values in the Big Endian format.
-	/// </summary>
-	public class BigEndianBinaryWriter : BinaryWriter
-	{		
-		/// <summary>
-		/// Initializes a new instance of the BigEndianBinaryWriter class.
-		/// </summary>
-		public BigEndianBinaryWriter() : base() { }
+    /// </summary>
+    public class BigEndianBinaryWriter : BinaryWriter
+    {
+        /// <summary>
+        /// Initializes a new instance of the BigEndianBinaryWriter class.
+        /// </summary>
+        public BigEndianBinaryWriter() : base() { }
 
-		/// <summary>
-		/// Initializes a new instance of the BigEndianBinaryWriter class 
+        /// <summary>
+        /// Initializes a new instance of the BigEndianBinaryWriter class
         /// based on the supplied stream and using UTF-8 as the encoding for strings.
-		/// </summary>
-		/// <param name="output">The supplied stream.</param>
-		public BigEndianBinaryWriter(Stream output) : base(output) { }
+        /// </summary>
+        /// <param name="output">The supplied stream.</param>
+        public BigEndianBinaryWriter(Stream output) : base(output) { }
 
-		/// <summary>
-		/// Initializes a new instance of the BigEndianBinaryWriter class 
+        /// <summary>
+        /// Initializes a new instance of the BigEndianBinaryWriter class
         /// based on the supplied stream and a specific character encoding.
-		/// </summary>
-		/// <param name="output">The supplied stream.</param>
-		/// <param name="encoding">The character encoding.</param>
-		public BigEndianBinaryWriter(Stream output, Encoding encoding): base(output,encoding) { }
+        /// </summary>
+        /// <param name="output">The supplied stream.</param>
+        /// <param name="encoding">The character encoding.</param>
+        public BigEndianBinaryWriter(Stream output, Encoding encoding): base(output, encoding) { }
 
-		/// <summary>
-		/// Reads a 4-byte signed integer using the big-endian layout from the current stream 
+        /// <summary>
+        /// Reads a 4-byte signed integer using the big-endian layout from the current stream
         /// and advances the current position of the stream by two bytes.
-		/// </summary>
+        /// </summary>
         /// <param name="value">The four-byte signed integer to write.</param>
-		public void WriteIntBE(int value)
-		{
+        public void WriteIntBE(int value)
+        {
             byte[] bytes = BitConverter.GetBytes(value);
             Debug.Assert(bytes.Length == 4);
 
             Array.Reverse(bytes, 0, 4);
-            Write(bytes);				
-		}
+            Write(bytes);
+        }
 
         /// <summary>
-        /// Reads a 8-byte signed integer using the big-endian layout from the current stream 
+        /// Reads a 8-byte signed integer using the big-endian layout from the current stream
         /// and advances the current position of the stream by two bytes.
         /// </summary>
         /// <param name="value">The four-byte signed integer to write.</param>
@@ -58,5 +58,5 @@ namespace NetTopologySuite.IO
             Array.Reverse(bytes, 0, 8);
             Write(bytes);
         }
-	}
+    }
 }
