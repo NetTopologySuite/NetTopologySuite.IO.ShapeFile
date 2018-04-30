6e82cd1dadea91babb826f79e47a88c44275dbf8
Joe Amenta <airbreather@linux.com>
2018-04-30 09:13:31 -0400

    Speed up ShapefileDataWriter.Write Accept IEnumerable<IFeature> instead of IList<IFeature>. Don't write a dummy .dbf file just to replace it. Write the .shp, .shx, and .dbf files all in a single forward scan.

--- NetTopologySuite.IO.GeoTools/ShapefileDataWriter.cs
+++ NetTopologySuite.IO.GeoTools/ShapefileDataWriter.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using GeoAPI.Geometries;
 using NetTopologySuite.Features;
-using NetTopologySuite.Geometries;
 using NetTopologySuite.IO.Streams;
 
 namespace NetTopologySuite.IO
@@ -165,7 +163,7 @@ namespace NetTopologySuite.IO
         /// Writes the specified feature collection.
         /// </summary>
         /// <param name="featureCollection">The feature collection.</param>
-        public void Write(IList<IFeature> featureCollection)
+        public void Write(IEnumerable<IFeature> featureCollection)
         {
             // Test if the Header is initialized
             if (Header == null)
@@ -173,36 +171,60 @@ namespace NetTopologySuite.IO
 
 #if DEBUG
             // Test if all elements of the collections are features
+            featureCollection = new List<IFeature>(featureCollection);
             foreach (object obj in featureCollection)
                 if (obj.GetType().IsAssignableFrom(typeof(IFeature)))
-                    throw new ArgumentException("All the elements in the given collection must be " + typeof(IFeature).Name);
+                    throw new ArgumentException("All the elements in the given collection must be " + typeof(IFeature).Name, nameof(featureCollection));
 #endif
 
-            try
+            using (IEnumerator<IFeature> featuresEnumerator = featureCollection.GetEnumerator())
             {
-                // Write shp and shx  
-                var geometries = new IGeometry[featureCollection.Count];
-                var index = 0;
-                foreach (IFeature feature in featureCollection)
-                    geometries[index++] = feature.Geometry;
-                ShapefileWriter.WriteGeometryCollection(_streamProviderRegistry, new GeometryCollection(geometries, _geometryFactory));
-
-                // Write dbf
-                _dbaseWriter.Write(Header);
-                var fieldNames = Array.ConvertAll(Header.Fields, field => field.Name);
-                var values = new object[fieldNames.Length];
-                foreach (IFeature feature in featureCollection)
+                // scan the original sequence looking for a geometry that we can
+                // use to figure out the shape type.  keep the original features
+                // around so we don't have to loop through the input twice; we
+                // shouldn't have to look *too* far for a non-empty geometry.
+                IGeometry representativeGeometry = null;
+                List<IFeature> headFeatures = new List<IFeature>();
+                while (representativeGeometry?.IsEmpty != false && featuresEnumerator.MoveNext())
                 {
-                    var attribs = feature.Attributes;
-                    for (int i = 0; i < fieldNames.Length; i++)
-                        values[i] = attribs[fieldNames[i]];
-                    _dbaseWriter.Write(values);
+                    IFeature feature = featuresEnumerator.Current;
+                    headFeatures.Add(feature);
+                    representativeGeometry = feature.Geometry;
                 }
-            }
-            finally
-            {
-                // Close dbf writer
-                _dbaseWriter.Close();
+
+                var shapeFileType = Shapefile.GetShapeType(representativeGeometry);
+                using (_dbaseWriter)
+                using (var shapefileWriter = new ShapefileWriter(_geometryFactory, _streamProviderRegistry, shapeFileType))
+                {
+                    _dbaseWriter.Write(Header);
+                    var fieldNames = Array.ConvertAll(Header.Fields, field => field.Name);
+                    var values = new object[fieldNames.Length];
+
+                    // first, write the one(s) that we scanned already.
+                    foreach (IFeature feature in headFeatures)
+                    {
+                        Write(feature);
+                    }
+
+                    // now continue through the features we haven't scanned yet.
+                    while (featuresEnumerator.MoveNext())
+                    {
+                        Write(featuresEnumerator.Current);
+                    }
+
+                    void Write(IFeature feature)
+                    {
+                        shapefileWriter.Write(feature.Geometry);
+
+                        var attribs = feature.Attributes;
+                        for (int i = 0; i < fieldNames.Length; i++)
+                        {
+                            values[i] = attribs[fieldNames[i]];
+                        }
+
+                        _dbaseWriter.Write(values);
+                    }
+                }
             }
         }
     }
