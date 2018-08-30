e90194e4d0e41a4e583677cc81db4f09254ddc88
Joe Amenta <airbreather@linux.com>
2018-08-30 08:34:22 -0400

    Perhaps this will fix Travis builds?

--- travis-build.sh
+++ travis-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-dotnet build -c Release
+dotnet msbuild NetTopologySuite.IO.ShapeFile.sln /m "/t:Restore;Build" /p:Configuration=Release "/p:FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/" /v:minimal /p:WarningLevel=3
 dotnet test NetTopologySuite.IO.ShapeFile.Test --no-build --no-restore -c Release
