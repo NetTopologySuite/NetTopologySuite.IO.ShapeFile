3cc8c7e4bf526207d1a6a6ba0ec99a0a7d63616e
Frank Hommers <frank-github@hommers.nl>
2018-09-11 15:32:53 +0200

    Made it an overload instead of optional parameter

--- NetTopologySuite.IO.GeoTools/ShapefileDataWriter.cs
+++ NetTopologySuite.IO.GeoTools/ShapefileDataWriter.cs
@@ -20,8 +20,7 @@ namespace NetTopologySuite.IO
         /// Gets the stub header.
         /// </summary>
         /// <param name="feature">The feature.</param>
-        /// <param name="count">The count.</param>
-        /// <param name="encoding">The encoding.</param>            
+        /// <param name="count">The count.</param>         
         /// <returns></returns>
         public static DbaseFileHeader GetHeader(IFeature feature, int count)
         {
